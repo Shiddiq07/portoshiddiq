e44e5b120b585d8e7b24ea84d2839c08bcba3bd8
muhammadshiddiq <mshiddiqs13@gmailcom>
2024-09-24 17:23:07 +0700

    kedua

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <>
       <div className="">
-        <h1 className="mt-24 text-6xl font-sans md:Apple Color Emoji">Hallo, </h1>
-        <h1 className=" text-6xl font-sans md:Apple Color Emoji text-yellow-600/100 " > Saya Jabal Jala Raga </h1>
-        <h1 className=" text-6xl font-sans md:Apple Color Emoji" >Data Analys</h1>
+        <h1 className="mt-24 text-6xl font-sans md:Apple Color Emoji">Hai, </h1>
+        <h1 className=" text-6xl font-sans md:Apple Color Emoji text-yellow-600/100 " > Saya Muhammad Shiddiq</h1>
+        <h1 className=" text-6xl font-sans md:Apple Color Emoji" >Android  Developer</h1>
         <div>
           <button className=" mt-20 rounded-lg bg-red-500 w-48 min-h-12 text-white" >Get Started</button>
         </div>
@@ -13,7 +13,7 @@ export default function Home() {
       <h2 className="mt-20 rounded-lg text-center text-3xl w-full">Tentang Saya</h2>
             
             <p className="text-center margin-0 mx-auto w-2/3	">
-                Saya adalah mahasiswa semester 5 bernama jabal jala raga dengan jurusan manajemen informatika tinkat 3 yang kepribadian baik
+                Saya adalah mahasiswa semester 5 jurusan manajemen informatika tingkat 3 
             </p>
 
     </>
