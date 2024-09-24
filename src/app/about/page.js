b44cb571a6c7c589d3bc965af075675a43374fbe
muhammadshiddiq <mshiddiqs13@gmailcom>
2024-09-24 17:24:57 +0700

    ketiga

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,23 @@
 export default function About() {
     return (
+      <>
       <div className="">
-       <h1>
-          Hallo, nama saya shiddiq
-       </h1>
+        <h1 className="mt-24 text-6xl font-sans md:Apple Color Emoji">Hai, </h1>
+        <h1 className=" text-6xl font-sans md:Apple Color Emoji text-yellow-600/100 " > Saya Muhammad Shiddiq</h1>
+        <h1 className=" text-6xl font-sans md:Apple Color Emoji" >Android  Developer</h1>
+        <div>
+          <button className=" mt-20 rounded-lg bg-red-500 w-48 min-h-12 text-white" >Get Started</button>
+        </div>
       </div>
+      
+      <h2 className="mt-20 rounded-lg text-center text-3xl w-full">Tentang Saya</h2>
+            
+            <p className="text-center margin-0 mx-auto w-2/3	">
+                Saya adalah mahasiswa semester 5 jurusan manajemen informatika tingkat 3 
+            </p>
+
+    </>
+    
     );
 }
   
